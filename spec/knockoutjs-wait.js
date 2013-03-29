69780544f2ed007f98d466260089f3227c0f3ee5
Vincent Bonmalais <vb.kouno@gmail.com>
2013-03-29 16:29:44 +1100

    Add an example of asynchronous content.
    A computed value can still be processing.

--- spec/knockoutjs-wait.js
+++ spec/knockoutjs-wait.js
@@ -1,32 +1,50 @@
 describe('KnockoutJS-Wait', function() {
-  var container;
+  var container, slow_runner;
 
   beforeEach(function() {
-    container = $('<div class="container" data-bind="text: count()"> </div>');
-    container.appendTo('body');
+    container      = $('<div class="container" data-bind="text: count()"> </div>');
+    slow_container = $('<div class="slow-container" data-bind="text: slow_count()"> </div>');
 
-    ko.applyBindings(SlowRunnerViewModel);
+    container.appendTo('body');
+    slow_container.appendTo('body');
+
+    slow_runner = new SlowRunnerViewModel()
+
+    ko.applyBindings(slow_runner);
   });
 
   afterEach(function() {
     container.remove();
+    slow_container.remove();
   });
 
   describe('Configuration', function () {
     it('has no content by default', function() {
-      expect(container.html()).toBe("0");
+      expect(container.html()).toBe('0');
     });
 
     it('has a value that mirrors our number of items', function() {
-      SlowRunnerViewModel.count(5);
-
+      slow_runner.count(5);
       expect(container.html()).toBe('5');
-    })
+      expect(slow_container.html()).toBe('5');
+    });
+
+    it('has a synchronisation problem', function() {
+      container.html('5');
+      expect(container.html()).toBe('5');
+      expect(slow_container.html()).toBe('0');
+    });
   });
 
   describe('Listening Process', function() {
     it('defines a listener', function() {
       expect(ko.bindingsApplied()).toBeTruthy();
     });
-  })
+
+    it('tells when bindings are still executing', function() {
+      container.html('5');
+      expect(slow_container.html()).toBe('0');
+      expect(ko.bindingsApplied()).toBeFalsy();
+    });
+  });
 });
--- spec/support/slow_runner_view_model.js
+++ spec/support/slow_runner_view_model.js
@@ -1,3 +1,17 @@
-var SlowRunnerViewModel = {
-  count: ko.observable(0)
+function SlowRunnerViewModel() {
+  this.count = ko.observable(0);
+
+  this.slow_count = ko.computed(function() {
+    this.sleep(100);
+    return this.count();
+  }, this);
+};
+
+SlowRunnerViewModel.prototype.sleep = function(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
 };
